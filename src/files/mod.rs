11016fc47d5ed4a1b1e3b5cbe18e0898b7ac38ac
Khenziii <khenzii.was.taken@gmail.com>
2024-02-28 21:15:00 +0100

    feat: add get_length_of_audio_file function

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -1,7 +1,39 @@
 use std::fs;
 use std::path::Path;
+use metadata::media_file::MediaFileMetadata;
 
-pub fn get_files(from_dir: &str) -> Vec<String> {
+/// converts a string in this format: "HH:MM:SS.ss"
+/// (where HH = hours, MM = minutes, SS = seconds, ss = milliseconds)
+/// to a number of milliseconds
+fn convert_duration_to_milliseconds_from_string(string: String) -> i32 {
+    let parts: Vec<&str> = string.split(":").collect();
+    if parts.len() == 3 {
+        let error_message = "Something went wrong while parsing";
+
+        let hours = parts[0]
+            .parse::<i32>()
+            .expect(&*format!("{} hours. Tried to parse: {}", error_message, parts[0]));
+        let minutes = parts[1]
+            .parse::<i32>()
+            .expect(&*format!("{} minutes. Tried to parse: {}", error_message, parts[1]));
+
+        let seconds_and_milliseconds: Vec<&str> = parts[2]
+            .split(".")
+            .collect();
+        let seconds = seconds_and_milliseconds[0]
+            .parse::<i32>()
+            .expect(&*format!("{} seconds. Tried to parse: {}", error_message, seconds_and_milliseconds[0]));
+        let milliseconds = seconds_and_milliseconds[1]
+            .parse::<i32>()
+            .expect(&*format!("{} milliseconds. Tried to parse: {}", error_message, seconds_and_milliseconds[1]));
+
+        return hours * 1000 * 60 * 60 + minutes * 1000 * 60 + seconds * 1000 + milliseconds;
+    }
+
+    panic!("Failed to convert the duration String to i32.");
+}
+
+fn get_files(from_dir: &str) -> Vec<String> {
     let files = fs::read_dir(from_dir)
         .expect(&*format!("Failed to read contents of the directory {}!", from_dir))
         .filter_map(|entry| {
@@ -49,4 +81,15 @@ pub fn validate_env(input_dir: &str, output_dir: &str) {
             panic!("File: {} is missing in the {} directory", file, input_dir);
         }
     }
+}
+
+pub fn get_length_of_audio_file(file_path: &str) -> i32 {
+    let metadata = MediaFileMetadata::new(&Path::new(file_path)).unwrap();
+    let duration_string: String = match metadata.duration {
+        Some(s) => s,
+        None => panic!("The audio file is probably corrupted."),
+    };
+    let duration_milliseconds = convert_duration_to_milliseconds_from_string(duration_string);
+
+    return duration_milliseconds;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod files;
 
 fn main() {
     files::validate_env("./input", "./output");
-    let files = files::get_files("./input");
+    let audio_file_duration = files::get_length_of_audio_file("./input/audio.mp3");
 }
