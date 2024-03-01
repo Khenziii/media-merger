853efc03720d7dd6f64b49ecfb1e565a23df12bc
Khenziii <khenzii.was.taken@gmail.com>
2024-03-01 18:49:57 +0100

    feat: simplify split_video func

--- src/video/mod.rs
+++ src/video/mod.rs
@@ -2,14 +2,14 @@ use crate::files::get_length_of_file;
 use std::process::{Command, ExitStatus, exit};
 use std::io;
 
-fn split_video(start_milliseconds: i32, end_milliseconds: i32, input_file: &String, output_file: &String) -> ExitStatus {
+fn split_video(start_milliseconds: i32, length_milliseconds: i32, input_file: &String, output_file: &String) -> ExitStatus {
     let status = Command::new("ffmpeg")
         .arg("-i")
         .arg(input_file)
         .arg("-ss")
         .arg(format!("{}", start_milliseconds / 1000))
-        .arg("-to")
-        .arg(format!("{}", end_milliseconds / 1000))
+        .arg("-t")
+        .arg(format!("{}", length_milliseconds / 1000))
         .arg(output_file)
         .status()
         .expect("Something went wrong while creating video's segments");
@@ -40,9 +40,8 @@ pub fn split_video_to_equal_parts(video: String, output_dir: String, part_length
 
     for i in 1..amount_of_videos_to_generate {
         let start_time = (i - 1) * part_length;
-        let end_time = start_time + part_length;
         let output_filename = format!("{}/{}.mp4", output_dir, i);
 
-        split_video(start_time, end_time, &video, &output_filename);
+        split_video(start_time, part_length, &video, &output_filename);
     }
 }
